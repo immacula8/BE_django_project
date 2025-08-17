80e4a8582317e7c9b14029ea4be85530a94d7d5f
Immaculate <geoffreyimmaculate@gmail.com>
2025-08-17 03:50:41 +0100

    Add Author model and link Book model via ForeignKey

--- Library_Management_System/books/models.py
+++ Library_Management_System/books/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
+    biography = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
     isbn = models.CharField(max_length=13, unique=True)
     published_date = models.DateField()
     copies_available = models.PositiveIntegerField()
